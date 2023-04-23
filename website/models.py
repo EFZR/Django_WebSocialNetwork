eaa9b2795dec1f5a308da13ea1bbe1cae14e71da
efzr <emerson.zapatarivas@gmail.com>
2023-04-22 22:44:14 -0600

    Worked with the template of login, Groups are needed to be create everything else

--- website/models.py
+++ website/models.py
@@ -1,3 +1,101 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    rate = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
+
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    chatroom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
+
+
+class Friend(models.Model):
+    class stateRequest(models.TextChoices):
+        PENDING = 'pending'
+        ACCEPTED = 'accepted'
+        REJECTED = 'rejected'
+        BLOCKED = 'blocked'
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='user'
+
+    )
+    friend = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='friend'
+    )
+
+    state = models.CharField(
+        max_length=20, choices=stateRequest.choices,
+        default=stateRequest.PENDING
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Notification(models.Model):
+    class typeNotification(models.TextChoices):
+        FRIEND_REQUEST = 'friend_request'
+        FRIEND_ACCEPTED = 'friend_accepted'
+        FRIEND_REJECTED = 'friend_rejected'
+        FRIEND_BLOCKED = 'friend_blocked'
+        MESSAGE_RECEIVED = 'message_received'
+        COMMENT_RECEIVED = 'comment_received'
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_notification')
+
+    notification_type = models.CharField(
+        max_length=20, choices=typeNotification.choices)
+
+    message_id = models.ForeignKey(
+        Message, on_delete=models.CASCADE, related_name='message_notification', null=True)
+
+    Friend_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='friend_notification', null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
--- website/views.py
+++ website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormView
 
 # Create your views here.
+
+
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = 'website/home.html'
